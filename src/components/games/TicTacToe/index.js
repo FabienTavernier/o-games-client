8124f3b80f3f26dd28464f3bac6513a007404e00
Fabien TAVERNIER <fabien.tavernier@oclock.io>
2023-02-05 12:45:32 +0100

    feat(TicTacToe): detect Win

--- src/components/games/TicTacToe/index.js
+++ src/components/games/TicTacToe/index.js
@@ -5,18 +5,27 @@ import Cell from './Cell';
 
 function TicTacToe({ numberOfRows }) {
   const [board, setBoard] = useState(Array(numberOfRows * numberOfRows).fill(null));
-  const [xTurn, toggleXTurn] = useState(true);
+  const [xTurn, setXTurn] = useState(true);
+  const [hasWin, setHasWin] = useState(null);
 
   function handleCellClick(i) {
-    if (board[i]) {
+    if (board[i] || hasWin) {
       return;
     }
 
+    const symbol = xTurn ? 'x' : 'o';
+
     const updatedBoard = [...board];
-    updatedBoard[i] = xTurn ? 'x' : 'o';
+    updatedBoard[i] = symbol;
 
     setBoard(updatedBoard);
-    toggleXTurn(!xTurn);
+
+    if (detectWin(updatedBoard, i)) {
+      setHasWin(symbol);
+    }
+    else {
+      setXTurn(!xTurn);
+    }
   }
 
   function getRows(number) {
@@ -52,12 +61,84 @@ function TicTacToe({ numberOfRows }) {
     return cellsElements;
   }
 
+  function detectWin(board, cellIndex) {
+    const symbol = board[cellIndex];
+
+    // Check if an array contains only the same symbol
+    const checkSymbols = (arr) => arr.every((cell) => cell && cell === symbol);
+
+    // Check if the row contains only the same symbol
+    const checkRow = (arr) => {
+      // which row?
+      const rowStart = parseInt(cellIndex / numberOfRows, 10) * numberOfRows;
+      const rowEnd = rowStart + numberOfRows;
+      const row = arr.slice(rowStart, rowEnd);
+
+      // are the symbols the same?
+      return checkSymbols(row);
+    };
+
+    // Check if the column contains only the same symbol
+    const checkColumn = (arr) => {
+      // get the modulo
+      const modulo = cellIndex % numberOfRows;
+      // get the column
+      const column = arr.filter((_, index) => index % numberOfRows === modulo);
+
+      // are the symbols the same?
+      return checkSymbols(column);
+    };
+
+    // Check if the left to right diagonal contains only the same symbol
+    const checkLeftDiagonal = (arr) => {
+      // get the diagonal
+      const diagonal = [];
+
+      for (let i = 0; i < numberOfRows; i++) {
+        diagonal.push(arr[i * (numberOfRows + 1)]);
+      }
+
+      // are the symbols the same?
+      return checkSymbols(diagonal);
+    };
+
+    // Check if the right to left diagonal contains only the same symbol
+    const checkRightDiagonal = (arr) => {
+      // get the diagonal
+      const diagonal = [];
+
+      for (let i = 1; i < numberOfRows + 1; i++) {
+        diagonal.push(arr[i * (numberOfRows - 1)]);
+      }
+
+      // are the symbols the same?
+      return checkSymbols(diagonal);
+    };
+
+    return (
+      checkRow(board)
+      || checkColumn(board)
+      || checkLeftDiagonal(board)
+      || checkRightDiagonal(board)
+    );
+  }
+
   return (
     <div className="game tic-tac-toe container">
       <h2>Tic-Tac-Toe</h2>
 
       <div className="board">
         {getRows(numberOfRows)}
+      </div>
+
+      <div className="status">
+        {hasWin && (
+          <p>Player <strong>{hasWin}</strong> wins!</p>
+        )}
+
+        {!hasWin && (
+          <p>Next player: {xTurn ? 'x' : 'o'}</p>
+        )}
       </div>
     </div>
   );
